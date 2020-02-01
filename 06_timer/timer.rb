1fe19645fbcc5d3671d647373ab6ef3cfbe2c780
Simeon Revales <simeonrevales@gmail.com>
2020-02-01 18:21:19 -0500

    Updated Timer code to be more efficient

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -5,19 +5,11 @@ class Timer
           @seconds = 0;
       end
 
-      def hours(seconds)
-        hours = seconds/3600
-        return hours
-      end
-      
-      def minutes(seconds)
-        minutes = seconds/60 % 60
-        return minutes
-      end
-
-
       def time_string
-        return "%02d:%02d:%02d" % [hours(seconds), minutes(seconds), seconds % 60]
+
+        hours = @seconds/3600
+        minutes = @seconds/60%60
+        return "%02d:%02d:%02d" % [hours, minutes, seconds % 60]
        end
 
 end
